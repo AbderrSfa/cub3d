b1960151a9cfb8d5f4ea028532525251dd376426
Abderrahmane Sfaihi <asfaihi@e1r7p6.1337.ma>
2020-10-27 14:35:41 +0100

    Key hold and release

--- world_map.c
+++ world_map.c
@@ -8,8 +8,8 @@ void	set_player(t_player *player)
 	player->turn_dir = 0;
 	player->walk_dir = 0;
 	player->rotation_angle = M_PI / 2;
-	player->move_speed = 3.0;
-	player->rotation_speed = 3 * (M_PI / 180);
+	player->move_speed = 6.0;
+	player->rotation_speed = 6 * (M_PI / 180);
 }
 
 void	draw_square(void *mlx_ptr, void *win_ptr, int x, int y)
@@ -63,23 +63,66 @@ void	draw_player(void *mlx_ptr, void *win_ptr, t_player *player)
 		i = 0;
 		while (i < player->radius)
 		{
-			mlx_pixel_put(mlx_ptr, win_ptr, player->x + player->turn_dir + i, player->y + player->walk_dir + j, 0xFFFFFF);
+			mlx_pixel_put(mlx_ptr, win_ptr, player->x + i, player->y + j, 0xFFFFFF);
 			i++;
 		}
 		j++;
 	}
 }
 
-int		deal_key(int key, t_player *player)
+int		has_wall(t_player *player)
+{
+	if (player->x < 0 || player->x > WIN_WIDTH || player->y < 0 || player->y > WIN_HEIGHT)
+		return (1);
+	if (world_map[(player->y / TILE_SIZE)][(player->x / TILE_SIZE)] == 1)
+		return (1);
+	return (0);
+}
+
+void	update_player(void *mlx_ptr, void *win_ptr, t_player *player)
+{
+	double	move_step;
+	double	temp_x;
+	double	temp_y;
+
+	move_step = player->walk_dir * player->move_speed;
+	player->rotation_angle += player->turn_dir * player->rotation_speed;
+	temp_x = player->x + cos(player->rotation_angle) * move_step;
+	temp_y = player->y + sin(player->rotation_angle) * move_step;
+/* 	if (!has_wall(player))
+	{ */
+		player->x = temp_x;
+		player->y = temp_y;
+/* 	} */
+	draw_player(mlx_ptr, win_ptr, player);
+}
+
+int		key_pressed(int key, t_player *player)
 {
 	if (key == 126)
-		player->walk_dir -= 8;
-	else if (key == 125)
-		player->walk_dir += 8;
-	else if (key == 124)
-		player->turn_dir += 8;
+		player->walk_dir = -1;
 	else if (key == 123)
-		player->turn_dir -= 8;
+		player->turn_dir = -1;
+	else if (key == 125)
+		player->walk_dir = 1;
+	else if (key == 124)
+		player->turn_dir = 1;
+	draw_map(player->mlx, player->win);
+	mlx_clear_window(player->mlx, player->win);
+	update_player(player->mlx, player->win, player);
+	return (0);
+}
+
+int		key_released(int key, t_player *player)
+{
+	if (key == 126)
+		player->walk_dir = 0;
+	else if (key == 125)
+		player->walk_dir = 0;
+	else if (key == 124)
+		player->turn_dir = 0;
+	else if (key == 123)
+		player->turn_dir = 0;
 	draw_map(player->mlx, player->win);
 	mlx_clear_window(player->mlx, player->win);
 	draw_player(player->mlx, player->win, player);
@@ -103,6 +146,7 @@ int     main()
 	set_player(&player);
 	draw_map(mlx_ptr, win_ptr);
 	draw_player(mlx_ptr, win_ptr, &player);
-	mlx_key_hook(win_ptr, deal_key, &player);
+	mlx_hook(win_ptr, 2, (1L<<0), key_pressed, &player);
+	mlx_hook(win_ptr, 3, (1L<<1), key_released, &player);
 	mlx_loop(mlx_ptr);
 }
